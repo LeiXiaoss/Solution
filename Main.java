5f6ee353c1016cf37b68620ebca218cfd2e34fd6
leixiao <2399520027@qq.com>
2018-08-12 21:20:07 +0800

    剑指 offer

--- Main.java
+++ Main.java
@@ -1131,6 +1131,22 @@ public class Main {
 //        System.out.println(new Main().GetNumberOfK2(new int[]{1,2,3,3,3,3,4,5},3));
 //    }
 
+    //输入一棵二叉树，求该树的深度。
+    // 从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，
+    // 最长路径的长度为树的深度。
+
+    //递归写法
+    public int TreeDepth(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+
+        return Math.max(left,right)+1;
+    }
+
 
 }
 
