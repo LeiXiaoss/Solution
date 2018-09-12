8049f615dc7d3280f4b3fe8b230b3e440f2bf9e4
leixiao <2399520027@qq.com>
2018-09-12 21:48:57 +0800

    剑指offer
    (cherry picked from commit 115b32d20e75dd46d0cf8ac9f3c7056d0de96c66)

--- Main.java
+++ Main.java
@@ -1778,6 +1778,39 @@ public class Main {
 //    public static void main(String[] args){
 //        System.out.println(new Main().MaxDiff(new int[]{9,11,8,5,7,12,16,14}));
 //    }
+
+    //给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+    // 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
+    // 不能使用除法。
+
+    public int[] multipiy(int[] A){
+        int length = A.length;
+
+        int[] B = new int[length];
+
+        int[] C = new int[length];
+        int[] D = new int[length];
+
+        C[0] = 1;
+        D[length-1] = 1;
+
+        for(int i=1;i<length;i++){
+            C[i] = A[i-1] * C[i-1];
+            D[length-1-i] = A[length-i] * D[length-i];
+        }
+
+        for(int i=0;i<length;i++){
+            B[i] = C[i] * D[i];
+        }
+
+        return B;
+    }
+
+//    public static void main(String[] args){
+//        System.out.println(new Main().multipiy(new int[]{1,2,3,4,5}));
+//    }
+
+
   }
 
 
