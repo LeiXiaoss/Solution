cf4bdf55680ad067534e55b1ede0fc138c4f451e
leixiao <2399520027@qq.com>
2018-09-26 17:00:53 +0800

    剑指offer
    (cherry picked from commit 115b32d20e75dd46d0cf8ac9f3c7056d0de96c66)

--- Main.java
+++ Main.java
@@ -2229,7 +2229,14 @@ public class Main {
     }
 
     //回溯法
+
     //矩阵中的路径
+    //请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。
+    // 路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，
+    // 向上，向下移动一个格子。如果一条路径经过了矩阵中的某一个格子，则之后不能再次进入这个格子。
+    // 例如 a b c e s f c s a d e e 这样的3 X 4 矩阵中包含一条字符串"bcced"的路径，
+    // 但是矩阵中不包含"abcb"路径，因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，
+    // 路径不能再次进入该格子。
 
     public boolean hasPath(char[] matrix,int rows,int cols,char[] str){
         if(matrix == null || rows<1 || cols<1 || str == null){
@@ -2283,6 +2290,60 @@ public class Main {
         }
         return result;
     }
+
+    //机器人的运动范围
+    //地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，
+    // 每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。
+    // 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。
+    // 但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+
+    public int movingCount(int threshold, int rows, int cols){
+        if(threshold<=0 || rows<=0 || cols<=0){
+            return 0;
+        }
+
+        boolean[] visited = new boolean[rows*cols];
+        for(int i=0;i<rows*cols;i++){
+            visited[i] = false;
+        }
+
+        int count = movingCountCore(threshold,rows,cols,0,0,visited);
+
+        return count;
+    }
+    public int movingCountCore(int threshold,int rows,int cols,
+                               int row,int col,boolean[] visited){
+        int count = 0;
+
+        if(check(threshold,rows,cols,row,col,visited)){
+            visited[row*cols+col] = true;
+
+            count =1+ movingCountCore(threshold,rows,cols,row,col+1,visited)+
+                    movingCountCore(threshold,rows,cols,row,col-1,visited)+
+                    movingCountCore(threshold,rows,cols,row+1,col,visited)+
+                    movingCountCore(threshold,rows,cols,row-1,col,visited);
+        }
+        return count;
+    }
+    public boolean check(int threshold,int rows,int cols,
+                         int row,int col,boolean[] visited){
+        if(row<rows&&col<cols&&row>=0&&col>=0&&
+                !visited[row*cols+col]&&getDigitSum(row)+getDigitSum(col)<=threshold){
+            return true;
+        }
+        return false;
+    }
+    public int getDigitSum(int num){
+        int sum = 0;
+
+        while(num>0){
+            sum += num%10;
+            num = num/10;
+        }
+
+        return sum;
+    }
+
   }
 
 
